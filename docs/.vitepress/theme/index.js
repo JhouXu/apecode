dfa2ad55454b95d28c78d8fe2c6894e9dc78ac58
JhouXu <2324837581@qq.com>
2023-03-27 15:41:03 +0800

    Feat: Aside 区域样式调整

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,6 +2,7 @@ import theme from "vitepress/theme";
 import "./tailwind.postcss";
 import "./styles/custom.scss";
 import "./styles/nav.scss";
+import "./styles/aside.scss";
 import NavCard from "../components/NavCard.vue";
 
 export default {
