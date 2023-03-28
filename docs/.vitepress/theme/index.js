eaf0ba30541b38924259bd2b0b663d829a2a9f72
JhouXu <2324837581@qq.com>
2023-03-28 17:56:40 +0800

    Feat: giscus 讨论功能

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,11 +4,36 @@ import "./styles/custom.scss";
 import "./styles/nav.scss";
 import "./styles/search.scss";
 import "./styles/aside.scss";
+
+import { useData, useRoute } from "vitepress";
+import giscusTalk from "vitepress-plugin-comment-with-giscus";
 import NavCard from "../components/NavCard.vue";
 
 export default {
   ...theme,
-  enhanceApp({ app }) {
-    app.component("NavCard", NavCard);
+  enhanceApp(ctx) {
+    theme.enhanceApp(ctx);
+    ctx.app.component("NavCard", NavCard);
+  },
+  setup() {
+    // 获取前言和路由
+    const { frontmatter } = useData();
+    const route = useRoute();
+    // 评论组件
+    giscusTalk(
+      {
+        repo: "JhouXu/apecode",
+        repoId: "R_kgDOJMzoMg",
+        category: "Announcements",
+        categoryId: "DIC_kwDOJMzoMs4CVW-J",
+        mapping: "pathname",
+        inputPosition: "top",
+        loading: "lazy",
+      },
+      {
+        frontmatter,
+        route,
+      }
+    );
   },
 };
