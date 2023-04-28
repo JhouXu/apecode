f285cca0f39230b15e4222c5679b5efc349ea6ea
JhouXu <2324837581@qq.com>
2023-04-28 10:52:45 +0800

    Fix: 增加文件操作收藏夹

--- docs/.vitepress/data/favorites/file.ts
+++ docs/.vitepress/data/favorites/file.ts
@@ -8,6 +8,12 @@ const file: navItem[] = [
     link: "https://convertio.co/zh/",
     desc: "将您的文件转换成任意格式",
   },
+  {
+    id: id++,
+    text: "图像压缩",
+    link: "https://docsmall.com/image-compress",
+    desc: "无需改变图片尺寸，极大压缩图片体积，视觉效果几乎一致",
+  },
 ];
 
 export default file;
