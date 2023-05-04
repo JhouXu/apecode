f36772be589a1d73cc9ab02fee81207515f80eae
JhouXu <2324837581@qq.com>
2023-05-04 17:58:37 +0800

    Fix blog 增加 plugin 分类

--- docs/.vitepress/config/blog.ts
+++ docs/.vitepress/config/blog.ts
@@ -3,16 +3,14 @@ export const blog = [
   { text: "微信小程序解析 markdown", link: "/blog/program-markdown", type: "小程序", time: "2023-05-01" },
   { text: "CSS3 转换过渡动画", link: "/blog/css-animation", type: "css", time: "2023-05-01" },
   { text: "Canvas 学习笔记", link: "/blog/js-canvas", type: "javascript", time: "2023-05-01" },
-  { text: "Vitepress 实战应用", link: "/blog/vue-vitepress", type: "javascript", time: "2023-05-01" },
-  { text: "SwiperJS 入门及实现常见的轮播效果", link: "/blog/js-swiper", type: "javascript", time: "2023-05-01" },
+  { text: "Vitepress 实战应用", link: "/blog/vue-vitepress", type: "plugin", time: "2023-05-01" },
+  { text: "SwiperJS 入门及实现常见的轮播效果", link: "/blog/js-swiper", type: "plugin", time: "2023-05-01" },
   {
     text: "PannellumJS 快速搭建属于你的全景查看器",
     link: "/blog/js-pannellum",
-    type: "javascript",
+    type: "plugin",
     time: "2023-05-01",
   },
-  { text: "Nuxt 填坑集合（面向实际开发解决方案）", link: "/blog/js-nuxt", type: "javascript", time: "2023-05-01" },
+  { text: "Nuxt 填坑集合（面向实际开发解决方案）", link: "/blog/js-nuxt", type: "plugin", time: "2023-05-01" },
   { text: "微信小程序中实现定位以及逆地址解析", link: "/blog/program-position", type: "小程序", time: "2023-05-01" },
 ];
-
-
