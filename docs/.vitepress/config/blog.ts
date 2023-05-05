dd5f3ff028df2092a69692e4e3ba34a458f9a384
JhouXu <2324837581@qq.com>
2023-05-05 10:02:34 +0800

    Fix 文章发布日期填写

--- docs/.vitepress/config/blog.ts
+++ docs/.vitepress/config/blog.ts
@@ -5,7 +5,24 @@ const example = "案例";
 const plugin = "扩展";
 
 export const blog = [
+  { text: "微信小程序之分享转发", link: "/blog/program-share", type: "小程序", time: "2023-04-30" },
+  { text: "微信小程序解析 markdown", link: "/blog/program-markdown", type: "小程序", time: "2023-04-30" },
+  { text: "Vitepress 实战应用", link: "/blog/vue-vitepress", type: `${plugin}`, time: "2023-01-01" },
+
+  { text: "SwiperJS 入门及实现常见的轮播效果", link: "/blog/js-swiper", type: `${plugin}`, time: "2022-12-18" },
+  {
+    text: "PannellumJS 快速搭建属于你的全景查看器",
+    link: "/blog/js-pannellum",
+    type: `${plugin}`,
+    time: "2022-08-07",
+  },
+  { text: "Nuxt 填坑集合（面向实际开发解决方案）", link: "/blog/js-nuxt", type: `${plugin}`, time: "2022-02-13" },
+
+  { text: "微信小程序中实现定位以及逆地址解析", link: "/blog/program-position", type: "小程序", time: "2021-12-30" },
+  { text: "CSS3 转换过渡动画", link: "/blog/css-animation", type: `${css}`, time: "2021-08-03" },
+  { text: "Canvas 学习笔记", link: "/blog/js-canvas", type: `${javascript}`, time: "2021-03-26" },
   { text: "页面锚点快速定位跳转", link: "/blog/example-anchor", type: `${example}`, time: "2021-02-25" },
+  { text: "PreloadJS 预加载及实现进度加载效果", link: "/blog/js-preload", type: `${plugin}`, time: "2021-02-23" },
   { text: "Sass 动态转换单位", link: "/blog/css-scss-unit", type: `${css}`, time: "2021-02-23" },
   { text: "Sass 的四种编译方式", link: "/blog/css-scss-compile", type: `${css}`, time: "2021-02-21" },
   { text: "audio 自动播放产生的困扰解决方案", link: "/blog/example-audio", type: `${example}`, time: "2021-02-20" },
@@ -16,7 +33,7 @@ export const blog = [
     time: "2021-02-07",
   },
   { text: "浏览器对象模型 BOM", link: "/blog/js-bom", type: `${javascript}`, time: "2021-01-29" },
-  { text: "PreloadJS 预加载及实现进度加载效果", link: "/blog/js-preload", type: `${plugin}`, time: "2021-02-23" },
+
   { text: "标准盒模型与怪异盒模型的区别", link: "/blog/css-box-sizing", type: `${css}`, time: "2020-12-24" },
   { text: "浮动清除的四大主要方式", link: "/blog/css-clear-float", type: `${css}`, time: "2020-12-23" },
   {
@@ -26,18 +43,4 @@ export const blog = [
     time: "2020-08-28",
   },
   { text: "元素水平垂直居中的六种方式", link: "/blog/css-center", type: `${css}`, time: "2020-08-28" },
-  { text: "微信小程序之分享转发", link: "/blog/program-share", type: "小程序", time: "2023-05-01" },
-  { text: "微信小程序解析 markdown", link: "/blog/program-markdown", type: "小程序", time: "2023-05-01" },
-  { text: "CSS3 转换过渡动画", link: "/blog/css-animation", type: `${css}`, time: "2023-05-01" },
-  { text: "Canvas 学习笔记", link: "/blog/js-canvas", type: `${javascript}`, time: "2023-05-01" },
-  { text: "Vitepress 实战应用", link: "/blog/vue-vitepress", type: `${plugin}`, time: "2023-05-01" },
-  { text: "SwiperJS 入门及实现常见的轮播效果", link: "/blog/js-swiper", type: `${plugin}`, time: "2023-05-01" },
-  {
-    text: "PannellumJS 快速搭建属于你的全景查看器",
-    link: "/blog/js-pannellum",
-    type: `${plugin}`,
-    time: "2023-05-01",
-  },
-  { text: "Nuxt 填坑集合（面向实际开发解决方案）", link: "/blog/js-nuxt", type: `${plugin}`, time: "2023-05-01" },
-  { text: "微信小程序中实现定位以及逆地址解析", link: "/blog/program-position", type: "小程序", time: "2023-05-01" },
 ];
