33e4ab36836bc70c1ed2b538dd0fea12f6c5313a
JhouXu <2324837581@qq.com>
2023-05-05 09:51:30 +0800

    Fix type 使用变量赋值的方式，统一处理

--- docs/.vitepress/config/blog.ts
+++ docs/.vitepress/config/blog.ts
@@ -1,37 +1,43 @@
+const javascript = "javascript";
+const css = "css";
+const program = "小程序";
+const example = "案例";
+const plugin = "扩展";
+
 export const blog = [
-  { text: "页面锚点快速定位跳转", link: "/blog/demo-anchor", type: "demo", time: "2021-02-21" },
-  { text: "Sass 动态转换单位", link: "/blog/css-scss-unit", type: "css", time: "2021-02-21" },
-  { text: "Sass 的四种编译方式", link: "/blog/css-scss-compile", type: "css", time: "2021-02-21" },
-  { text: "audio 自动播放产生的困扰解决方案", link: "/blog/demo-audio", type: "demo", time: "2021-02-20" },
+  { text: "页面锚点快速定位跳转", link: "/blog/example-anchor", type: `${example}`, time: "2021-02-21" },
+  { text: "Sass 动态转换单位", link: "/blog/css-scss-unit", type: `${css}`, time: "2021-02-21" },
+  { text: "Sass 的四种编译方式", link: "/blog/css-scss-compile", type: `${css}`, time: "2021-02-21" },
+  { text: "audio 自动播放产生的困扰解决方案", link: "/blog/example-audio", type: `${example}`, time: "2021-02-20" },
   {
     text: "html2canvas 实现截图附上模糊处理方案",
     link: "/blog/js-html2canvas",
-    type: "javascript",
+    type: `${javascript}`,
     time: "2021-02-07",
   },
-  { text: "浏览器对象模型 BOM", link: "/blog/js-bom", type: "javascript", time: "2021-01-29" },
-  { text: "PreloadJS 预加载及实现进度加载效果", link: "/blog/js-preload", type: "plugin", time: "2021-02-23" },
-  { text: "标准盒模型与怪异盒模型的区别", link: "/blog/css-box-sizing", type: "css", time: "2020-12-24" },
-  { text: "浮动清除的四大主要方式", link: "/blog/css-clear-float", type: "css", time: "2020-12-23" },
+  { text: "浏览器对象模型 BOM", link: "/blog/js-bom", type: `${javascript}`, time: "2021-01-29" },
+  { text: "PreloadJS 预加载及实现进度加载效果", link: "/blog/js-preload", type: `${plugin}`, time: "2021-02-23" },
+  { text: "标准盒模型与怪异盒模型的区别", link: "/blog/css-box-sizing", type: `${css}`, time: "2020-12-24" },
+  { text: "浮动清除的四大主要方式", link: "/blog/css-clear-float", type: `${css}`, time: "2020-12-23" },
   {
     text: "CSS 中实现隐藏",
     link: "/blog/css-hidden",
-    type: "css",
+    type: `${css}`,
     time: "2020-08-28",
   },
-  { text: "元素水平垂直居中的六种方式", link: "/blog/css-center", type: "css", time: "2020-08-28" },
+  { text: "元素水平垂直居中的六种方式", link: "/blog/css-center", type: `${css}`, time: "2020-08-28" },
   { text: "微信小程序之分享转发", link: "/blog/program-share", type: "小程序", time: "2023-05-01" },
   { text: "微信小程序解析 markdown", link: "/blog/program-markdown", type: "小程序", time: "2023-05-01" },
-  { text: "CSS3 转换过渡动画", link: "/blog/css-animation", type: "css", time: "2023-05-01" },
-  { text: "Canvas 学习笔记", link: "/blog/js-canvas", type: "javascript", time: "2023-05-01" },
-  { text: "Vitepress 实战应用", link: "/blog/vue-vitepress", type: "plugin", time: "2023-05-01" },
-  { text: "SwiperJS 入门及实现常见的轮播效果", link: "/blog/js-swiper", type: "plugin", time: "2023-05-01" },
+  { text: "CSS3 转换过渡动画", link: "/blog/css-animation", type: `${css}`, time: "2023-05-01" },
+  { text: "Canvas 学习笔记", link: "/blog/js-canvas", type: `${javascript}`, time: "2023-05-01" },
+  { text: "Vitepress 实战应用", link: "/blog/vue-vitepress", type: `${plugin}`, time: "2023-05-01" },
+  { text: "SwiperJS 入门及实现常见的轮播效果", link: "/blog/js-swiper", type: `${plugin}`, time: "2023-05-01" },
   {
     text: "PannellumJS 快速搭建属于你的全景查看器",
     link: "/blog/js-pannellum",
-    type: "plugin",
+    type: `${plugin}`,
     time: "2023-05-01",
   },
-  { text: "Nuxt 填坑集合（面向实际开发解决方案）", link: "/blog/js-nuxt", type: "plugin", time: "2023-05-01" },
+  { text: "Nuxt 填坑集合（面向实际开发解决方案）", link: "/blog/js-nuxt", type: `${plugin}`, time: "2023-05-01" },
   { text: "微信小程序中实现定位以及逆地址解析", link: "/blog/program-position", type: "小程序", time: "2023-05-01" },
 ];
