78ed0cf49026f1086cc6e1a0c168e11e04ffa006
JhouXu <2324837581@qq.com>
2023-05-18 23:18:56 +0800

    Fix 更新文件操作地址

--- docs/.vitepress/data/favorites/file.ts
+++ docs/.vitepress/data/favorites/file.ts
@@ -10,9 +10,15 @@ const file: navItem[] = [
   },
   {
     id: id++,
+    text: "ILovePdf🔥",
+    link: "https://www.ilovepdf.com/zh-cn",
+    desc: "PDF爱好者的在线工具。完全免费、易于使用、丰富的PDF处理工具，包括：合并、拆分、压缩、转换、旋转和解锁PDF文件，以及给PDF文件添加水印的工具等。仅需几秒钟即可完成。",
+  },
+  {
+    id: id++,
     text: "SmallPdf",
     link: "https://smallpdf.com/cn",
-    desc: "PDF 文件的所有在线操作。",
+    desc: "PDF 文件的所有在线操作。目前收费。",
   },
 ];
 
