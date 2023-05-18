9f2d2b04afe042a04ef9d9e50be996066fec79cd
JhouXu <2324837581@qq.com>
2023-05-18 14:36:41 +0800

    Fix NavCard 鼠标悬浮效果

--- docs/.vitepress/components/NavCard.vue
+++ docs/.vitepress/components/NavCard.vue
@@ -11,7 +11,7 @@ defineProps<{
     <section v-for="navItem of navData" :key="navItem.id">
       <a :href="navItem.link" rel="noreferrer" target="_blank" class="group">
         <section
-          class="flex h-full flex-col border-[1px] border-solid border-gray-300 dark:border-gray-600 rounded-[8px] leading-[24px] px-[24px] py-[12px] group-hover:shadow"
+          class="flex h-full flex-col border-[1px] border-solid border-gray-300 dark:border-gray-600 rounded-[8px] leading-[24px] px-[24px] py-[12px] group-hover:shadow group-hover:border-[#62bbee] dark:group-hover:border-[#589fe8]"
         >
           <span
             class="text-gray-400 group-hover:text-gray-600 dark:text-gray-400 dark:group-hover:text-gray-200 transition duration-150 ease-in-out"
