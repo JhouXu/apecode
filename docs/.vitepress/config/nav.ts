708119fa4ca606c56acbe3fb9f1ffcca8d658812
JhouXu <2324837581@qq.com>
2023-10-11 10:08:36 +0800

    Del 移除导航栏首页入口

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -2,7 +2,6 @@ import { blog } from "../data/blog/blog";
 import navBlog from "./navBlog";
 
 export const nav: Array<Object> = [
-  { text: "首页", link: "/" },
   { text: "随笔记", items: [...navBlog] },
   {
     text: "日常工具",
