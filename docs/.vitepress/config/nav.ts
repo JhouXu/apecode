70b6cabbf2ed73cb59ed9e953d2f7125d7758c47
JhouXu <2324837581@qq.com>
2023-10-30 12:33:37 +0800

    Feat 导航栏增加代码块入口

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -5,4 +5,5 @@ export const nav: Array<Object> = [
   { text: "首页", link: "/" },
   { text: "导航", link: "/pages/navigation" },
   { text: "随笔记", items: [...navBlog] },
+  { text: "代码块", items: "/block" },
 ];
