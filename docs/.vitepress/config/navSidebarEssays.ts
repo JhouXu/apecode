22b21be4287dd8c0c329526bef58bbfef57747a9
jhouxu <2324837581@qq.com>
2024-01-08 16:56:31 +0800

    Fix 八股文页面初始化

--- docs/.vitepress/config/navSidebarEssays.ts
+++ docs/.vitepress/config/navSidebarEssays.ts
@@ -2,4 +2,7 @@ export const navSidebarEssays = [
   { text: "概述", link: "/pages/essays/essays.md" },
   { text: "网络", link: "/pages/essays/network.md" },
   { text: "浏览器", link: "/pages/essays/browser.md" },
+  { text: "HTML", link: "/pages/essays/html.md" },
+  { text: "CSS", link: "/pages/essays/css.md" },
+  { text: "JavaScript", link: "/pages/essays/javascript.md" },
 ];
