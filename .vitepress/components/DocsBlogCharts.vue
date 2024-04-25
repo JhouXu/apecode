dc55b9368aba9dd3bb9f49b5e7b1366efe0a911f
jhouxu <2324837581@qq.com>
2024-04-25 15:17:59 +0800

    Bug 编辑器 ts 语法检测异常

--- .vitepress/components/DocsBlogCharts.vue
+++ .vitepress/components/DocsBlogCharts.vue
@@ -14,11 +14,11 @@ let chartHeatmap: echarts.ECharts; // 热力图实例
 
 let heatmapTheme = {
   "": {
-    color: ["#f1f1f1", "#dae2ef", "#c0ddf9", "#73b3f3", "#3886e1", "#17459e"],
+    colors: ["#f1f1f1", "#dae2ef", "#c0ddf9", "#73b3f3", "#3886e1", "#17459e"],
     borderColor: "#e2e2e3",
   },
   dark: {
-    color: ["#100c2a", "#dae2ef", "#c0ddf9", "#73b3f3", "#3886e1", "#17459e"],
+    colors: ["#100c2a", "#dae2ef", "#c0ddf9", "#73b3f3", "#3886e1", "#17459e"],
     borderColor: "#2e2e32",
   },
 };
