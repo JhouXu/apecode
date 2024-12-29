79a0fdcb20d84deaac69760e66ccaa12aa9a335e
jhouxu <2324837581@qq.com>
2024-12-29 23:47:22 +0800

    fix Items 组件样式调整

--- docs/literature/components/Items.vue
+++ docs/literature/components/Items.vue
@@ -31,26 +31,29 @@ const getIndex = function (nowIndex: number) {
 <template>
   <div class="list">
     <div class="item" v-for="(item, index) in getData" :key="index">
-      <div class="container">
-        <span class="badge">{{ getIndex(index) }}、</span>
-        <span class="content">{{ item.content }}</span>
+      <div class="number">{{ getIndex(index) }}、</div>
+
+      <div class="content">
+        <span class="cn">{{ item.content }}</span>
+        <br />
+        <span class="en" v-if="item.contentEn">{{ item.contentEn }}</span>
+        <hr />
       </div>
-      <div v-if="item.contentEn">
-        {{ item.contentEn }}
-      </div>
+
       <div class="info">
-        <div class="source">
+        <div class="info-source">
           {{ item.source }}
           <span v-if="item.profession"> - </span>
           {{ item.profession }}
         </div>
-        <div class="date">收录于：{{ item.date }}</div>
+
+        <div class="info-date">收录于：{{ item.date }}</div>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 :root {
   --tips-color: #096;
 }
@@ -63,70 +66,65 @@ const getIndex = function (nowIndex: number) {
   display: flex;
   flex-direction: column;
   gap: 24px;
-}
 
-.item {
-  display: block;
-  padding: 20px;
-  height: 100%;
-  background-color: var(--vp-c-bg-soft);
-  border: 1px solid var(--vp-c-bg-soft);
-  border-radius: 12px;
-  transition: border-color 0.25s, background-color 0.25s, box-shadow 0.25s;
-}
+  .item {
+    position: relative;
+    display: block;
+    padding: 20px;
+    height: 100%;
+    background-color: var(--vp-c-bg-soft);
+    border: 1px solid var(--vp-c-bg-soft);
+    border-radius: 12px;
+    transition: border-color 0.25s, background-color 0.25s, box-shadow 0.25s;
 
-.item:hover {
-  border-color: var(--vp-c-brand-1);
-  box-shadow: 0 0 5px 2px var(--vp-c-brand-3);
-}
+    .content {
+      text-align: justify;
 
-.container {
-  text-align: justify;
-  font-size: 20px;
-  line-height: 1.8;
-  letter-spacing: 1px;
-}
+      .cn {
+        font-size: 20px;
+        line-height: 1.8;
+        letter-spacing: 1px;
+      }
 
-span.badge {
-  font-family: "AlimamaShuHeiTi";
-}
+      .en {
+        font-size: 15px;
+        line-height: 1.8;
+        font-weight: bold;
+      }
 
-span.content {
-  background: linear-gradient(to right bottom, var(--quotation-color2), var(--quotation-color1));
-  background-repeat: no-repeat;
-  background-position: right bottom;
-  background-size: 0px 2px;
-  transition: background-size 0.5s;
-}
+      span.cn,
+      span.en {
+        background: linear-gradient(to right bottom, var(--quotation-color2), var(--quotation-color1));
+        background-repeat: no-repeat;
+        background-position: right bottom;
+        background-size: 0px 2px;
+        transition: background-size 0.5s;
+      }
+    }
 
-.item:hover span.content {
-  background-position: left bottom;
-  background-size: 100% 2px;
-}
+    .info {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      margin-top: 10px;
+      font-size: 14px;
+    }
+  }
 
-.info {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  margin-top: 10px;
-}
+  .item:hover {
+    border-color: var(--vp-c-brand-1);
+    box-shadow: 0 0 5px 2px var(--vp-c-brand-3);
+  }
 
-.source {
-  font-size: 14px;
-  color: var(--tips-color);
-}
+  .item:hover span.cn,
+  .item:hover span.en {
+    background-position: left bottom;
+    background-size: 100% 2px;
+  }
 
-span.content:hover {
-  background-size: 100% 2px;
-}
-
-.info {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  margin-top: 10px;
-  font-size: 14px;
+  span.content:hover {
+    background-size: 100% 2px;
+  }
 }
 </style>
