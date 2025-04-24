4cfadd58fd09d2bf00edf122c0df34a167de0d1a
jhouxu <2324837581@qq.com>
2025-04-24 15:30:48 +0800

    refactor(vitepress): optimize markdown transform plugin

--- .vitepress/plugins/markdownTransform.ts
+++ .vitepress/plugins/markdownTransform.ts
@@ -1,56 +1,30 @@
 import type { Plugin } from "vite";
-import { replacer } from "../../scripts/utils";
 import { getReadingTime } from "../utils";
 
 export function MarkdownTransform(): Plugin {
   return {
     name: "apecode-md-transform",
     enforce: "pre",
-    async transform(code, id) {
-      if (!id.match(/\.md\b/)) return null;
-      // convert links to relative
-      code = code.replace(/https?:\/\/apecode\.cn\//g, "/");
-      const [_name, i] = id.split("/").slice(-2);
+    async transform(code, path) {
+      if (!path.match(/\.md\b/)) return null;
+      const [_router, fileName] = path.split("/").slice(-2);
 
-      // // convert img
-      // const imgRegex = /!\[(.+?)\]\((.+?)\)/g;
-      // let imgMatches = imgRegex.exec(code);
-      // while (imgMatches) {
-      //   const [text, link] = imgMatches.slice(1);
-      //   code = code.replace(imgMatches[0], `<img src="${link}" alt="${text || "img"}" />`);
-      //   imgMatches = imgRegex.exec(code);
-      // }
+      // Whitelist
+      if (
+        (_router === "docs" && fileName === "index.md") ||
+        (_router === "docs" && fileName === "navigation.md") ||
+        (_router === "blog" && fileName === "blog.md") ||
+        (_router === "essays" && fileName === "essays.md") ||
+        _router === "literature"
+      ) {
+        return code;
+      }
 
-      // // convert links to components
-      // const linkRegex = /\[(.+?)\]\((.+?)\)/g;
-      // let matches = linkRegex.exec(code);
-      // while (matches) {
-      //   const [text, link] = matches.slice(1);
-      //   code = code.replace(matches[0], `<CustomLink title="${text}" href="${link}" />`);
-      //   matches = linkRegex.exec(code);
-      // }
-
-      // cut index.md
-      if (_name === "docs" && i === "index.md") return code;
-
-      // const { footer } = await getDocsMarkdown();
-      // code = replacer(code, footer, "FOOTER", "tail");
+      // Add PageInfo
       const { readTime, words } = getReadingTime(code);
-      console.log(readTime, words);
       code = code.replace(/^#\s.+$/gm, `$&\n\n<PageInfo readingTime="${readTime}" wordCount="${words}"/>\n`);
 
       return code;
     },
-  };
-}
-
-export async function getDocsMarkdown() {
-  const ContributorsSection = `## Contributors
-  <Contributors/>`;
-  const CopyRightSection = `
-  <CopyRight/>`;
-  const footer = `${ContributorsSection}\n${CopyRightSection}\n`;
-  return {
-    footer,
   };
 }
