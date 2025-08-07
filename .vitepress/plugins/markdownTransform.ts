87fbd531cedf88da6cf400f9435881199c8df1ab
jhouxu <2324837581@qq.com>
2025-08-07 22:57:32 +0800

    fix 移除白名单，调整逻辑顺序

--- .vitepress/plugins/markdownTransform.ts
+++ .vitepress/plugins/markdownTransform.ts
@@ -12,27 +12,30 @@ export function MarkdownTransform(): Plugin {
       if (!path.match(/\.md\b/)) return null;
       const [_router, fileName] = path.split("/").slice(-2);
 
-      // Whitelist
-      if (
-        (_router === "docs" && fileName === "index.md") ||
-        (_router === "docs" && fileName === "navigation.md") ||
-        _router === "literature"
-      ) {
-        return code;
-      }
+      // // Whitelist
+      // if (
+      //   (_router === "docs" && fileName === "index.md") ||
+      //   (_router === "docs" && fileName === "navigation.md") ||
+      //   _router === "literature"
+      // ) {
+      //   return code;
+      // }
 
       // 过滤 markdown code "# "
       const codeBlocks = code.match(/```[\s\S]*?```/g) || [];
       const placeholders = codeBlocks.map((_, i) => `__CODE_BLOCK_${i}__`);
       code = code.replace(/```[\s\S]*?```/g, (match) => placeholders.shift() || match);
 
-      // Add PageInfo
+      // Blog 的文章发布时间存储在 BlogData 中，需要解析出来
       const BD = BlogData.reduce<BlogItem[]>((acc, item) => {
         if (item.items) acc.push(...item.items);
         return acc;
       }, []);
-      const { readTime, words } = getReadingTime(code);
       const publicTime = getPublicTime(BD, `${_router}/${fileName}`);
+
+      const { readTime, words } = getReadingTime(code);
+
+      // Add PageInfo
       code = code.replace(
         /^#\s.+$/gm,
         `$&\n\n<PageInfo readingTime="${readTime}" wordCount="${words}" publicTime="${publicTime}"/>\n`
