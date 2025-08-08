cf740c66fb355d3c08ad685726ae4aeebc7031b5
jhouxu <2324837581@qq.com>
2025-08-08 23:37:48 +0800

    feat PageInfo 增加 tags 字段数据

--- .vitepress/components/PageInfo.vue
+++ .vitepress/components/PageInfo.vue
@@ -1,26 +1,36 @@
 <template>
-  <div class="article-info">
-    <div class="info-item">
-      <span class="label">🧑🏻‍💻 作者：</span>
-      <span class="value">{{ author }}</span>
-    </div>
-    <div class="info-item">
-      <span class="label">🗓️ 发布时间：</span>
-      <span class="value">{{ publishDate }}</span>
-    </div>
-    <div class="info-item">
-      <span class="label">🕘 最后更新：</span>
-      <span class="value">{{ lastUpdatedFromNow }}</span>
-    </div>
-    <div class="info-item">
-      <span class="label">🔢 字数统计：</span>
-      <span class="value">{{ wordCount }}</span>
-    </div>
-    <div class="info-item">
-      <span class="label">📖 预计阅读：</span>
-      <span class="value">{{ readingTime }}</span>
-      <span class="label">分钟</span>
-    </div>
+  <div class="page-info-container">
+    <section class="info">
+      <div class="info-item">
+        <span class="label">🧑🏻‍💻 作者：</span>
+        <span class="value">{{ author }}</span>
+      </div>
+      <div class="info-item">
+        <span class="label">🗓️ 发布时间：</span>
+        <span class="value">{{ publishDate }}</span>
+      </div>
+      <div class="info-item">
+        <span class="label">🕘 最后更新：</span>
+        <span class="value">{{ lastUpdatedFromNow }}</span>
+      </div>
+      <div class="info-item">
+        <span class="label">🔢 字数统计：</span>
+        <span class="value">{{ wordCount }}</span>
+      </div>
+      <div class="info-item">
+        <span class="label">📖 预计阅读：</span>
+        <span class="value">{{ readingTime }}</span>
+        <span class="label">分钟</span>
+      </div>
+    </section>
+
+    <hr v-show="tags.length > 0" />
+
+    <section class="tags" v-show="tags.length > 0">
+      <template v-for="tag in tags" :key="tag">
+        <Badge type="tip" :text="tag"></Badge>
+      </template>
+    </section>
   </div>
 </template>
 
@@ -49,31 +59,41 @@ const publishDate = computed(() => {
 const lastUpdatedFromNow = computed(() => {
   return getFromNow(page.value.lastUpdated || Date.now());
 });
+
+const tags = computed(() => {
+  return frontmatter.value?.tags || [];
+});
 </script>
 
-<style scoped>
-.article-info {
+<style lang="scss" scoped>
+.page-info-container {
   margin: 1rem 0;
   padding: 1rem;
   background-color: var(--vp-c-bg-soft);
   border-radius: 8px;
-  display: flex;
-  flex-wrap: wrap;
-  gap: 1rem;
-}
 
-.info-item {
-  display: flex;
-  align-items: center;
-  font-size: 0.9rem;
-}
+  section.info {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
 
-.label {
-  color: var(--vp-c-text-2);
-}
+    .info-item {
+      display: flex;
+      align-items: center;
+      font-size: 0.9rem;
 
-.value {
-  color: var(--vp-c-text-1);
-  font-weight: 500;
+      .label {
+        color: var(--vp-c-text-2);
+      }
+
+      .value {
+        color: var(--vp-c-text-1);
+        font-weight: 500;
+      }
+    }
+  }
+
+  section.tags {
+  }
 }
 </style>
